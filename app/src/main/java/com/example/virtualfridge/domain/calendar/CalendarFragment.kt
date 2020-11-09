12a2b59b564b300edc5b15a602f7b8f5fbdc1609
KurekWioletta <kurek.wioletta97@gmail.com>
2020-11-09 20:17:07 +0100

    add calendar view

--- app/src/main/java/com/example/virtualfridge/domain/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/virtualfridge/domain/calendar/CalendarFragment.kt
@@ -1,11 +1,22 @@
 package com.example.virtualfridge.domain.calendar
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.virtualfridge.R
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import java.time.YearMonth
+import java.time.temporal.WeekFields
+import java.util.*
 
 class CalendarFragment : Fragment() {
     override fun onCreateView(
@@ -16,7 +27,34 @@ class CalendarFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_calendar, parent, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            // Called only when a new container is needed.
+            override fun create(view: View) = DayViewContainer(view)
+
+            // Called every time we need to reuse a container.
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.textView.text = day.date.dayOfMonth.toString()
+                container.textView.setOnClickListener {
+                    Toast.makeText(view.context, "adfasdf", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        val currentMonth = YearMonth.now()
+        val firstMonth = currentMonth.minusMonths(10)
+        val lastMonth = currentMonth.plusMonths(10)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
+        calendarView.scrollToMonth(currentMonth)
+    }
+
+    class DayViewContainer(view: View) : ViewContainer(view) {
+        val textView = view.findViewById<TextView>(R.id.calendarDayText)
+
+        // With ViewBinding
+        // val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
     }
 }
