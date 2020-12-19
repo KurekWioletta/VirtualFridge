0e46311f7553303070eb21b40b34b0afd1cacfec
Wioletta Kurek <wioletta.kurek@azimo.com>
2020-12-19 23:48:49 +0100

    calendarview

--- app/src/main/java/com/example/virtualfridge/domain/calendar/CalendarFragmentPresenter.kt
+++ app/src/main/java/com/example/virtualfridge/domain/calendar/CalendarFragmentPresenter.kt
@@ -28,14 +28,19 @@ class CalendarFragmentPresenter @Inject constructor(
 ) {
     private val cachedEvents = mutableMapOf<LocalDate, MutableList<EventViewModel>>()
 
+    // przy emisji podobnie jak w BoardActivity bedziemy odswiezac kalendarz, ale mozna tez przekazac parametr
+    // przy emisji eventu, tym parametrem bedzie id wybranego czlonka rodziny, zeby pokazac kalendarz dla wybranego czlonka
     private val triggerRefresh = BehaviorSubject.createDefault<String>("")
     private val monthYearFormatter = DateTimeFormatter.ofPattern("MMM yyyy")
     private val selectedDayFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
 
     fun init() = view.registerViewSubscription(
+        // zestawiamy dwa observable w jeden, zeby moc jednoczesnie handlowac errory z dwoch sygnałów
         Observable.combineLatest(
+            // pobranie czlonkow rodziny
             familyApi.familyMembers(userDataStore.loggedInUser().id)
                 .map { fromResponse(it) },
+            // pobranie wydarzen dla odpowiedniego czlonka rodziny z systemem odswiezania jak w boardActivity
             triggerRefresh.flatMap { userId ->
                 eventsApi.events(
                     if (userId.isEmpty()) {
@@ -44,20 +49,31 @@ class CalendarFragmentPresenter @Inject constructor(
                         userId
                     }
                 )
+                    // mapowanie na EventViewModel
                     .map { EventViewModel.fromResponse(it) }
+                    // wrzucamy powyzsze operatory na IO thread, a ponizsze na mainThread
                     .compose { rxTransformerManager.applyIOScheduler(it) }
             },
+            // Pair(first, last) = first to last
             BiFunction { family: List<FamilyMemberViewModel>, events: List<EventViewModel> -> family to events }
         )
+            // wrzucamy powyzsze operatory na IO thread, a ponizsze na mainThread
             .compose { rxTransformerManager.applyIOScheduler(it) }
             .doOnSubscribe { view.showLoading() }
             .doOnEach { view.hideLoading() }
+            //  (first, last) = Pair()
             .subscribe({ (family, events) ->
+                // wrzucamy powyzsze operatory na IO thread, a ponizsze na mainThread
                 view.updateFamilyMembers(family)
+                // usuniecie zcachowanych danych
                 cachedEvents.clear()
+                // przeiterowanie sie po eventach i dla kazdego eventu dodanie go do cache
                 events.forEach {
+                    // zeby wyswietlil sie uevent ktory trwa przez 5 dni to trzeba dodatkowo dodac
+                    // go do naszej mapy w kazdy dzien w ktorym wydarzenie ma miec miejsce
                     val startDate = LocalDate.parse(it.startDate, dateTimeFormatter)
                     val endDate = LocalDate.parse(it.endDate, dateTimeFormatter)
+                    // dlatego iterujemy po dacie startDate to endDate
                     for (date in startDate.rangeTo(endDate)) {
                         if (cachedEvents.contains(date)) {
                             cachedEvents[date]!!.add(it)
@@ -66,6 +82,7 @@ class CalendarFragmentPresenter @Inject constructor(
                         }
                     }
                 }
+                // update eventow, dla kalendarza potrzebna jest tylko informacja czy w danym dniu sa eventy (zeby pokazac kropke)
                 view.updateEventsOnCalendar(cachedEvents.mapValues { true })
             }, {
                 view.showAlert(apiErrorParser.parse(it))
@@ -73,6 +90,7 @@ class CalendarFragmentPresenter @Inject constructor(
     )
 
     fun triggerRefresh(userId: String) {
+        // przekazujemy parametr userId wyemitowanemu eventowi
         triggerRefresh.onNext(userId)
     }
 
@@ -84,6 +102,7 @@ class CalendarFragmentPresenter @Inject constructor(
                 date, selectedDayFormatter.format(date)
             )
             if (cachedEvents.containsKey(date)) {
+                // update listy wydarzen na podstawie zachowanych danych
                 view.updateEventsList(cachedEvents[view.selectedDate]!!)
             }
         }
